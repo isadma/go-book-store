222fe21680e2611b7cfd579ff0f7a818fe739af9
Maksat <m.meredov@iqfulfillment.com>
2023-02-10 15:29:21 +0400

    app file

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,22 @@
 package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	connection, err := gorm.Open("mysql", "root:/bookstore?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = connection
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
