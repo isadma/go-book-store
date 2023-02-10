f1e42db9bbdecf0b64615d590735f9bb3ac9986d
Maksat <m.meredov@iqfulfillment.com>
2023-02-10 15:58:54 +0400

    crud functions on model

--- pkg/models/Book.go
+++ pkg/models/Book.go
@@ -8,8 +8,8 @@ import (
 var db *gorm.DB
 
 type Book struct {
-	gorm.model
-	Name        string `gorm:""json:"name"`
+	gorm.Model
+	Name        string `gorm:"" json:"name"`
 	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
@@ -18,4 +18,28 @@ func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func Index() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func Show(id int64) (*Book, *gorm.DB) {
+	var book Book
+	db := db.Where("ID=?", id).Find(&book)
+	return &book, db
+}
+
+func (book *Book) Create() *Book {
+	db.NewRecord(book)
+	db.Create(&book)
+	return book
+}
+
+func delete(id int64) Book {
+	var book Book
+	db.Where("ID=?", id).Delete(&book)
+	return book
 }
