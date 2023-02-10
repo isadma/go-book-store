22b7bf12ada13045c37d158220600ed4b2e11e13
Maksat <m.meredov@iqfulfillment.com>
2023-02-10 15:48:39 +0400

    main function

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/isadma/go-book-store/pkg/routes"
+)
+
 // import (
 
 // )
 
 func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
 
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
